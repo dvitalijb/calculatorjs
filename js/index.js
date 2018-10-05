b6b9f61d1c18ab9d73c7ce1ffeb0e2cc2fc611ba
vitalij <dumavb777@gmail.com>
2018-10-05 17:58:40 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -1,89 +1,78 @@
-let A, B, C;
-let action = "+";
+(function() {
 
-function getValue(id) {
-    let res = document.getElementById(id).value - 0;
-    if (!isNaN(res))
-        return res;
-    alert("Enter the number");
-}
+    let input = document.getElementById('input');
+    let button = document.querySelectorAll('#button');
 
-function PutNum(val) {
-    document.getElementById("windowput").value = (document.getElementById("windowput").value + val) - 0;
+    let inputValue = input.value;
+    let digitsone;
+    let znak;
+    let digitstwo;
 
-}
-
-function plus() {
-    A = getValue("windowput");
-    document.getElementById("windowput").value = 0;
-    action = "+";
-}
-
-function sub() {
-    A = getValue("windowput");
-    document.getElementById("windowput").value = 0;
-    action = "-";
-}
-
-function mult() {
-    A = getValue("windowput");
-    document.getElementById("windowput").value = 0;
-    action = "*";
-}
-
-function div() {
-    A = getValue("windowput");
-    document.getElementById("windowput").value = 0;
-    action = "/";
-}
-
-function clr() {
-    document.getElementById("windowput").value = 0;
-}
-
-function otr() {
-    A = getValue("windowput");
-    document.getElementById("windowput").value = 0 - A;
-}
-
-function calculate() {
-    B = getValue("windowput");
-    switch (action) {
-    case "+":
-        C = A + B;
-        break;
-    case "-":
-        C = A - B;
-        break;
-    case "*":
-        C = A * B;
-        break;
-    case "/":
-        if (B == 0) {
-            alert("Can not be divided into 0!");
-        } else {
-            C = A / B;
-        }
-        break;
+    for (i = 0; button.length; i++) {
+        button[i].addEventListener('click', cal);
     }
-    document.getElementById("windowput").value = C;
-}
 
-window.onload = function() {
-
-    for (let i = 0; i < 10; i++) {
-        (function(val) {
-            document.getElementById("but_" + val).onclick = function() {
-                PutNum(val);
+    function cal() {
+        if ((+(this.value)) >= 0 && (+(this.value)) <= 9) {
+            if (input.value === '+' || input.value.indexOf('=') !== (-1) || input.value === '-' || input.value === '*' || input.value === '/' || input.value.indexOf('Н') !== (-1)) {
+                input.value = '';
             }
+
+            input.value = input.value + this.value;
+
         }
-        )(i);
+        else if (this.value === 'delete') {
+            input.value = input.value.slice(0, -1);
+
+        }
+        else if (this.value === '+') {
+            digitsone = input.value;
+            input.value = '+';
+            znak = this.value;
+
+        } else if (this.value === '-') {
+            digitsone = input.value;
+            input.value = '-';
+            znak = this.value;
+
+        } else if (this.value === '*') {
+            digitsone = input.value;
+            input.value = '*';
+            znak = this.value;
+        } else if (this.value === '/') {
+            digitsone = input.value;
+            input.value = '/';
+            znak = this.value;
+        }
+        else if (this.value === '=') {
+            digitstwo = input.value;
+            if (znak === '+') {
+                input.value = '=' + ((+(digitsone)) + (+(digitstwo)));
+            }
+
+            if (znak === '-') {
+                input.value = '=' + ((+(digitsone)) - (+(digitstwo)));
+            }
+
+            if (znak === '*') {
+                input.value = '=' + ((+(digitsone)) * (+(digitstwo)));
+            }
+
+            if (znak === '/') {
+                if ((+(digitstwo)) == 0) {
+                    input.value = 'На нуль ділити не можна';
+
+                } else if ((+(digitstwo)) !== 0) {
+                    input.value = '=' + ((Number(digitsone)) / (Number(digitstwo)));
+                }
+
+            }
+
+        }
+        else if (this.value === 'C') {
+            input.value = '';
+        }
+
     }
-    document.getElementById("plus").addEventListener("click", plus);
-    document.getElementById("calculate").addEventListener("click", calculate);
-    document.getElementById("sub").addEventListener("click", sub);
-    document.getElementById("mult").addEventListener("click", mult);
-    document.getElementById("div").addEventListener("click", div);
-    document.getElementById("clr").addEventListener("click", clr);
-    document.getElementById("otr").addEventListener("click", otr);
 }
+)();
